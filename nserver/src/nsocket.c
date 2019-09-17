d141e11b0358dc13719ba7ca8f0ab3bcf5e77c3e
rsiddharth <s@ricketyspace.net>
2019-09-16 20:36:17 -0400

    nserver: nsocket.c: Move includes to nsocket.h.

--- nserver/src/nsocket.c
+++ nserver/src/nsocket.c
@@ -1,8 +1,3 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-
-#include <dbg.h>
 #include <nsocket.h>
 
 int get_socket()
--- nserver/src/nsocket.h
+++ nserver/src/nsocket.h
@@ -1,6 +1,12 @@
 #ifndef _nsocket_h
 #define _nsocket_h
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#include <dbg.h>
+
 #define PORT "7899"
 
 int get_socket();
