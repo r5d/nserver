8724cf43c27922c2a9eb10846bafbe2f3d8f7cef
rsiddharth <s@ricketyspace.net>
2019-09-29 19:08:30 -0400

    nserver: Add stats.h

---
+++ nserver/src/stats.h
@@ -0,0 +1,33 @@
+#ifndef stats_h
+#define stats_h
+#include <math.h>
+
+typedef struct Stats {
+    double sum;
+    double sumsq;
+    unsigned long n;
+    double min;
+    double max;
+} Stats;
+
+Stats *Stats_recreate(double sum, double sumsq, unsigned long n,
+                      double min, double max);
+
+Stats *Stats_create();
+
+void Stats_sample(Stats *st, double s);
+
+void Stats_dump(Stats *st);
+
+static inline double Stats_mean(Stats *st)
+{
+    return st->sum / st->n;
+}
+
+static inline double Stats_stddev(Stats *st)
+{
+    return sqrt((st->sumsq - (st->sum * st->sum / st->n)) /
+                (st->n - 1));
+}
+
+#endif
