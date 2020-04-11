239aec3926ca588d7e0c04ed3005741f4224b630
rsiddharth <s@ricketyspace.net>
2020-04-11 11:20:29 -0400

    nserver/src/db.c: Add mk_datum.
    * nserver/src/db.c (mk_datum): New function.

--- nserver/src/db.c
+++ nserver/src/db.c
@@ -1,12 +1,33 @@
 #include <db.h>
 
-
 static const char *DB_FILE = "nserver.db";
+
+datum *mk_datum(char *data)
+{
+    datum *d = NULL;
+
+    check(data != NULL, "data invalid");
+
+    d = calloc(1, sizeof(datum));
+    check(d != NULL, "datum mem alloc failed");
+
+    d->dptr = data;
+    d->dsize = strlen(data) + 2;
+
+    return d;
+ error:
+    if (d) {
+        free(d);
+    }
+    return NULL;
+}
+
 
 GDBM_FILE db_open(int flags)
 {
    return gdbm_open(DB_FILE, 0,flags, S_IRUSR|S_IWUSR, NULL);
 }
+
 
 int db_init()
 {
